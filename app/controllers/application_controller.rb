f1e2721286aaee171f7a497bde7d3aecd76f91f1
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-28 14:48:15 -0400

    stubbed out user model and user new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+  private
+
+  def require_login
+    redirect_to root_path unless session.include?(:user_id)
+  end
+
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  # before_action :require_login
+  # skip_before_action :require_login, only: [:new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+end
