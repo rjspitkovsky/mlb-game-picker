6917c310c32bdf2c82c739f77900c6e91327c30a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-29 17:05:32 -0400

    adding omniauth gem and login capabilities in user model with 3rd party authentication with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,21 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: {minimum: 6}
   validates :email, uniqueness: true
-  validates :username, presence: true 
+  validates :username, presence: true
+
+
+  def logged_in_through_facebook
+  !!self.uid
+  end
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    oauth_email = auth_hash['info']['email']
+    where(:email => oauth_email).first_or_create do |user|
+      user.password = SecureRandom.hex
+      user.username = auth_hash['info']['name']
+      user.uid = auth_hash['uid']
+    end
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  get '/auth/facebook/callback' => 'sessions#create'
 end
