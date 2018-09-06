762a27e503ad63f20c0bc0f6e5841fbab21474ad
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-09-06 16:37:43 -0400

    set up picks#create action

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,12 +1,22 @@
-require 'pry'
-class PicksController < ApplicationController
 
+class PicksController < ApplicationController
+  before_action :require_login
 
   def new
     @pick = Pick.new
+
   end
 
   def create
-    binding.pry 
+    @pick = Pick.new
+    @pick.date = Date.new
+    @pick.winners = params[:winners]
+    @pick.user_id = session[:user_id]
+
+     if @pick.save
+       redirect_to root_path
+     else
+       render :new
+     end
   end
 end
